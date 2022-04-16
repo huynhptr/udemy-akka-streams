9aaadcaa1deb732a12c385571bd8f58301472f41
peterhuynh7 <chuynh7@uic.edu>
2022-04-16 08:25:44 -0500

    23. Custom Operators with Graph Stages

---
+++ src/main/scala/part5_advanced/CustomOperators.scala
@@ -0,0 +1,90 @@
+package part5_advanced
+
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.{ActorMaterializer, Attributes, Inlet, Outlet, SinkShape, SourceShape}
+import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+
+import scala.collection.mutable
+import scala.util.Random
+
+object CustomOperators extends App {
+  implicit val system = ActorSystem("CustomOperators")
+  implicit val materializer = ActorMaterializer()
+
+  //1 - a custom source which emits random numbers until cancelled
+  class RandomNumberGenerator(max: Int) extends GraphStage[/*Step 0*/SourceShape[Int]] {
+
+    // step 1: define the ports and the component-specific members
+    val outPort = Outlet[Int]("randomGenerator")
+    val random = new Random()
+
+    // step 2 - construct a new shape
+    override def shape: SourceShape[Int] = SourceShape(outPort)
+
+    // step 3 - create the logic
+    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+      new GraphStageLogic(shape) {
+        // step 4 - define mutable state
+        //implement my logic here
+        setHandler(outPort, new OutHandler {
+          // when there is demand from downstream
+          override def onPull(): Unit = {
+            // emit a new element
+            val nextNumber = random.nextInt(max)
+            // push it out of the outport
+            push(outPort, nextNumber)
+          }
+        })
+      }
+  }
+
+  val randomGeneratorSource = Source.fromGraph(new RandomNumberGenerator(100))
+//  randomGeneratorSource.runWith(Sink.foreach(println))
+
+  // 2 - a custom sink that prints elements in batches of a given size
+
+  class Batcher(batchSize: Int) extends GraphStage[SinkShape[Int]] {
+    val inPort = Inlet[Int]("batcher")
+
+    override def shape: SinkShape[Int] = new SinkShape[Int](inPort)
+
+    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+      new GraphStageLogic(shape) {
+
+        override def preStart(): Unit = {
+          pull(inPort)
+        }
+
+        //mutable state
+        val batch = new mutable.Queue[Int]
+
+        setHandler(inPort, new InHandler {
+          //when the upstream wants to sent me an element
+          override def onPush(): Unit = {
+            val nextElement = grab(inPort)
+            batch.enqueue(nextElement)
+
+            //assume some complex computation ( for back pressure)
+            Thread.sleep(100)
+
+            if(batch.size >= batchSize) {
+              println("New batch: " + batch.dequeueAll(_ => true).mkString("[",", ","]"))
+            }
+
+            pull(inPort) // send demand upstream
+          }
+
+          override def onUpstreamFinish(): Unit = {
+            if (batch.nonEmpty) {
+              println("New batch: " + batch.dequeueAll(_ => true).mkString("[",", ","]"))
+              println("Stream finishes.")
+            }
+          }
+        })
+      }
+  }
+
+  val batcherSink = Sink.fromGraph(new Batcher(10))
+  randomGeneratorSource.to(batcherSink).run()
+}
